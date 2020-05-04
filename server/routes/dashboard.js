82a69818106b7349118cf8bbcae566e3c540aaf0
Birte Spreuer <mail@birtespreuer.com>
2020-05-04 16:00:19 +0200

    preparing for merge with admin-register

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const db = require("../database/models");
-<<<<<<< HEAD
-// const Admin = db.admin_login;
-=======
->>>>>>> feature-admin-register
 
 /**
  * GET for dashboard access

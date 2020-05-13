b9af97d6ebe3f038e4f47667a9a714bccec0cfd8
Elsa Desta <elsamenelik@gmail.com>
2020-05-13 15:36:47 +0200

    trendoverview section

--- client/src/components/TrendOverview.js
+++ client/src/components/TrendOverview.js
@@ -1,44 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import ProgressBar from "./ProgressBar";
+import TrendProgressBar from "./TrendProgressBar";
+import "../stylesheets/TrendOverview.scss";
 import calculateProgressBarPercent from "../helper/calculateProgressBarPercent";
 
-const blue = "#145cc6";
-const red = "#ff0013";
-const yellow = "#ffb000";
+
 
 export class TrendOverview extends Component {
   render() {
     return (
-      <div>
-        {/* {this.props.combined_votes? <p>{this.props.combined_votes.map((data) => <p > {data.tech_list.name} {data.subzero_votes} {data.cool_votes} {data.uncool_votes} 
-                 
-        </p>)} </p> : null} */}
-
+      <div className="trendBar--wrapper">
+        
         {this.props.combined_votes
           ? this.props.combined_votes.map((data, index) => (
-              <div>
-                {" "}
-                ( {data.tech_list.name}{" "}
-                <ProgressBar
-                  percent={calculateProgressBarPercent(data, data.uncool_votes)}
-                  color={yellow}
-                  name="UnCool"
-                />
-                <ProgressBar
-                  percent={calculateProgressBarPercent(data, data.cool_votes)}
-                  color={red}
-                  name="Cool"
-                />
-                <ProgressBar
-                  percent={calculateProgressBarPercent(
+              <div className="trendBar--content_wrapper">
+                <div className="trendBar--content_name"> {data.tech_list.name} </div>
+               <div className="trendBar--progressbar"> 
+                <TrendProgressBar
+                  percentUncool={calculateProgressBarPercent(
+                    data,
+                    data.uncool_votes
+                  )}
+                  percentCool={calculateProgressBarPercent(
+                    data,
+                    data.cool_votes
+                  )}
+                  percentSubZero={calculateProgressBarPercent(
                     data,
                     data.subzero_votes
                   )}
-                  color={blue}
-                  name="SubZero"
-                />{" "}
-                ){" "}
+                />
+                </div>
               </div>
             ))
           : null}

8bc2a1d2478f1dad6ff749225525eeeeada6130d
anu radha <anu.sivasubramanian@gmail.com>
2020-05-13 14:40:33 +0200

    test for gettingTechIcon ready

--- client/src/tests/gettingTechIcon.test.js
+++ client/src/tests/gettingTechIcon.test.js
@@ -1,31 +1,28 @@
-// import gettingTechIcon from "../helper/gettingTechIcon";
+import { gettingTechIcon } from "../helper/gettingTechIcon";
 
-// test("function returns the total votes when called with combined votes", () => {
-//   const mockcombined_votes = [
-//     {
-//       cool_votes: 2,
-//       uncool_votes: 1,
-//       subzero_votes: 4,
-//       tech_list: {
-//         name: "Express",
-//       },
-//     },
-//     {
-//       cool_votes: 2,
-//       uncool_votes: 3,
-//       subzero_votes: 5,
-//       tech_list: {
-//         name: "JQuery",
-//       },
-//     },
-//     {
-//       cool_votes: 2,
-//       uncool_votes: 3,
-//       subzero_votes: 6,
-//       tech_list: {
-//         name: "React",
-//       },
-//     },
-//   ];
-//   expect(calculateTotalVotes(mockcombined_votes)).toEqual(28);
-// });
+test("function returns the svg icon in string removing the escape characters", () => {
+  const mocktech_list = [
+    {
+      borderForSelectedVote: "none",
+      id: 1,
+      name: "Laveral",
+      svg: '<svg height="200" width="500"></svg>',
+    },
+    {
+      borderForSelectedVote: "none",
+      id: 2,
+      name: "React",
+      svg: '<svg height=\\"200" width="500"></svg>',
+    },
+    {
+      borderForSelectedVote: "none",
+      id: 3,
+      name: "Angular",
+      svg: '<svg height="200" width="500"></svg>',
+    },
+  ];
+  const mocktech_name = "React";
+  expect(gettingTechIcon(mocktech_list, mocktech_name)).toEqual(
+    '<svg height="200" width="500"></svg>'
+  );
+});

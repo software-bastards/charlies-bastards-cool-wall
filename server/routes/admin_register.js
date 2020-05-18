3609952d4c5f6328b2c86028f0789d99a2230dfb
anu radha <anu.sivasubramanian@gmail.com>
2020-05-18 16:49:46 +0200

    adding admin pwd change

--- server/routes/admin_register.js
+++ server/routes/admin_register.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const db = require("../database/models");
+const passport = require("passport");
 const Admin = db.admin_login;
 
 router.post("/", (req, res) => {
@@ -18,6 +19,30 @@ router.post("/", (req, res) => {
       })
         .then(() =>
           res.status(200).send({ message: "You've successfully signed up!" })
+        )
+        .catch((userErr) => console.error(`User error: ${userErr}`));
+    })
+    .catch((hashErr) => console.error(`Hashing gave errors: ${hashErr}`));
+});
+
+router.put("/", passport.authenticate("jwt", { session: false }), function (
+  req,
+  res
+) {
+  /* Hashing incoming password */
+  console.log(req.body);
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPassword) => {
+      /* Add the new user to the database table with hashedPassword. */
+      Admin.update(
+        { password: hashedPassword },
+        { where: { email: req.body.email } }
+      )
+        .then(() =>
+          res
+            .status(200)
+            .send({ message: "You've successfully changed the pssword!" })
         )
         .catch((userErr) => console.error(`User error: ${userErr}`));
     })
